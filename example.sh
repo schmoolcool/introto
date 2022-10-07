322a0dc55a420da138a4c8ec5a61ca6872efa059
lausen <lausen@tar.black>
2022-10-07 01:32:32 -0400

    Update and rename egs1006.sh to example.sh

--- example.sh
+++ example.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Comment/description for code
-
+# comment/description for code
 
 clear
 
-
 echo "hello world"
-
-
-
+sleep 1
 
 whiptail \
 --msgbox "you have opened this program" \
 --title "egs1006" \
 12 70
-
 
 {
 sleep 1
@@ -40,18 +35,21 @@ echo "75% complete..."
 echo XXX
 sleep 1
 echo XXX
+echo 99
+echo "99% complete..."
+echo XXX
+sleep 3
+echo XXX
 echo 100
 echo "100% complete. success."
 echo XXX
-sleep 3
+sleep 2
 } |
+
 whiptail \
 --gauge "the program is about to begin" \
 --title "loading..." \
 12 35 0
-
-
-
 
 whiptail \
 --inputbox "user:" \
